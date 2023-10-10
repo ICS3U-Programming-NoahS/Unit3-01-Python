707f2b9191e01f8faf8fd7d8187d0bd1e053ae51
Noah Smith <noah.smith@stu.ocsb.ca>
2023-10-10 23:31:03 +0000

    created tax.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# Created by: Noah Smith
+# Created on: Oct. 10th, 2023
+# This module contains constants.
+HST = 0.15
---
+++ tax.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Noah Smith
+# Created on: Oct. 10th, 2023
+# This program asks the user for the subtotal ($). It then calculates and displays
+# the total and the tax.
+import constants
+
+
+def main():
+    # Get the subtotal
+    subtotal = float(input("Enter the subtotal ($): "))
+
+    # Calculate the tax and total
+    tax = constants.HST * subtotal
+    total = subtotal + tax
+
+    # Display the total cost back to the user
+    print("")
+    print("The total is = ${:.2f}".format(total))
+    print("The tax is = ${:.2f}".format(tax))
+
+
+if __name__ == "__main__":
+    main()
